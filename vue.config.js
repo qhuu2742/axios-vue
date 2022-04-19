da2a00adf7d03fada9daae2fdd07ef1c671a4ffe
qhuu2742 <qhuu.official@gmail.com>
2022-04-19 10:01:51 +0700

    deploy

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/REPO_NAME/" : "/",
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/axios-vue/' // Thay tên repository của các bạn vào đây nhé
+    : '/'
 })
